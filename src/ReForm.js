db986dd4f1c4d05bf41acbe53e079ed2ddc0d480
Miroslav Nikolov <miroslavnikolov@ymail.com>
2020-03-20 16:09:18 +0100

    Make form submission manual. Consumer is now responsible on when to call form submission and what function to execute as a result.

--- src/ReForm.js
+++ src/ReForm.js
@@ -18,15 +18,22 @@ export default class ReForm extends PureComponent {
 
     this.handlers = {
       onChange: this.onChange.bind(this),
-      onValidateField: this.onValidateField.bind(this)
+      onSubmit: this.onSubmit.bind(this),
+      isDirty: this.isDirty.bind(this),
+      onValidateField: this.onValidateField.bind(this),
+      clearError: this.clearError.bind(this)
     };
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  validate() {
+  isDirty() {
+    const { initial } = this.props;
+    const fields = Object.keys(initial);
+    return fields.some(field => initial[field] !== this.state.values[field]);
+  }
+
+  validate(onValid = () => {}) {
     const { validation } = this.props;
     if (!validation) return true;
-
     const { values, errors } = this.state;
     const newErrors = {};
 
@@ -34,14 +41,19 @@ export default class ReForm extends PureComponent {
       newErrors[i] = validation[i](values);
     }
 
-    this.setState({
-      errors: {
-        ...errors,
-        ...newErrors
+    this.setState(
+      {
+        errors: {
+          ...errors,
+          ...newErrors
+        }
+      },
+      () => {
+        if (this.isValid()) {
+          onValid();
+        }
       }
-    });
-
-    return this.isValid();
+    );
   }
 
   onValidateField(name) {
@@ -76,14 +88,20 @@ export default class ReForm extends PureComponent {
     });
   }
 
-  handleSubmit(e) {
-    e.preventDefault();
-    const { onSubmit } = this.props;
-    if (onSubmit) {
-      const isValid = this.validate();
-      if (isValid) {
-        onSubmit(this.state.values);
+  clearError(name) {
+    this.setState({
+      errors: {
+        ...this.state.errors,
+        [name]: false
       }
+    });
+  }
+
+  onSubmit(submitCallback) {
+    if (typeof submitCallback === "function") {
+      this.validate(() => {
+        submitCallback(this.state.values);
+      });
     }
   }
 
@@ -118,7 +136,12 @@ export default class ReForm extends PureComponent {
     const { children, className } = this.props;
 
     return (
-      <form className={className || null} onSubmit={this.handleSubmit}>
+      <form
+        className={className || null}
+        onSubmit={e => {
+          e.preventDefault();
+        }}
+      >
         {children({ values, errors, ...this.handlers })}
       </form>
     );
@@ -129,6 +152,5 @@ ReForm.propTypes = {
   children: PropTypes.func.isRequired,
   initial: PropTypes.object.isRequired,
   className: PropTypes.string,
-  validation: PropTypes.object,
-  onSubmit: PropTypes.func
+  validation: PropTypes.object
 };
