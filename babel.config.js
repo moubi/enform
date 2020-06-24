46e2639cacddc03e7bdfbceaa1eda1c6651f9e0c
Miroslav Nikolov <miroslavnikolov@ymail.com>
2020-06-24 12:44:36 +0200

    Remove comments from the code on build.
    Babel transpilations options:
     - add IE11 support
     - ensure polyfills are loaded just once
     - transpile modules to commonjs in tests

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
 module.exports = {
-  presets: ["@babel/preset-env", "@babel/preset-react"]
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: { ie: 11 },
+        useBuiltIns: "usage"
+      }
+    ],
+    "@babel/preset-react"
+  ],
+  env: {
+    test: {
+      presets: [
+        [
+          "@babel/preset-env",
+          { targets: { node: "current" }, modules: "commonjs" }
+        ]
+      ]
+    }
+  }
 };
